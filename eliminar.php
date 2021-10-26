2db0dfbba9c16f7f536e79ec9d510b0b449ef9d6
yesicajv <yjv15.yjv@gmail.com>
2021-10-26 13:37:19 -0700

    modificacines en eliminar

--- eliminar.php
+++ eliminar.php
@@ -4,8 +4,7 @@ validar();
 
 $id=$_POST['column1'];
 
-$query="DELETE FROM table1 WHERE column1=$id;";
-
+//$query=;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -14,7 +13,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-if ( $conn->query($query)){
+if ( $conn->query("DELETE FROM table1 WHERE column1=$id;")){
     header("location:crud.php");
 }else{
     echo "Algo salio mal <a href='https://localhost/crud/crud.php'> clic aqui para volver al crud</a>" ;
